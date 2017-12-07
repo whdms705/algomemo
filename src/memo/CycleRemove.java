a6c3129fe36a1572ffb85f6879ecbabca80a50cd
whdms705 <whdms705@nate.com>
2017-12-08 07:54:43 +0900

    temp

--- src/memo/CycleRemove.java
+++ src/memo/CycleRemove.java
@@ -13,31 +13,60 @@ public class CycleRemove {
 		int d; // 방향   >> 1:시계방향 , 0:반시계방향
 		int k,j;// 얼만큼 이동
 		//System.out.println("몇명으로 시작한가요?");
-		n=sc.nextInt();
+		//n=sc.nextInt();
 		//System.out.println("방향을 입력하시오");
-		d=sc.nextInt();
+		//d=sc.nextInt();
 		//System.out.println("k,j입력");
-		k=sc.nextInt();
-		j=sc.nextInt();
+		//k=sc.nextInt();
+		//j=sc.nextInt();
+		n=8;
+		d=1;
+		k=2;
+		j=1;
 		solution(n,d,k,j);
 	}
 	
 	public static void solution(int n,int d,int k,int j){
-		System.out.println("----");
+		System.out.println(" ");
+		int preIndex=0;//현재 위치 저장을 위한 변수
 		ArrayList<Integer> list = new ArrayList<Integer>();
+		
 		for(int i=0;i<n;i++){
-			list.add(0);
+			list.add(i+1);
 		}
 		
+		listView(list);
+		
+		if(d==0){//반시계방향으로 
+			
+		}
+		if(d==1){//시계방향으로 
+			while(list.size()!=1){//한명이 남을 때까지 돈다.
+				System.out.println();
+				if(list.size()!=n){
+					preIndex=(preIndex+k+j)%list.size()-1;
+					k=k+j;
+					System.out.println("처음! : "+preIndex);
+				}else if(list.size()==n){
+					preIndex=(preIndex+k)%list.size();
+					System.out.println("처음 : "+preIndex);
+				}
+				list.remove(preIndex);
+				listView(list);
+			}
+		}
+		
+		
+		
+		
+	}
+	
+	// 원형안에 인원수 체크
+	public static void listView(ArrayList<Integer> list){
+		System.out.println();
 		for(int i : list){
-			System.out.println(">> "+i);
+			System.out.print(" >> "+i);
 		}
-		
-		
-		
-		
-		
-		
 	}
 
 }
