91fe70fb406285beac5bc6c27cf402371d0a9f70
whdms705 <whdms705@nate.com>
2017-12-09 20:49:09 +0900

    단어의 가운데 값 출력하기

---
+++ src/programers/WordGetmiddleEasy.java
@@ -0,0 +1,42 @@
+package programers;
+
+import java.util.Scanner;
+
+/*
+ * content -  단어를 입력 받아서 가운데 글자를 반환하도록 getMiddle메소드를 만들어 보세요. 
+ * 단어의 길이가 짝수일경우 가운데 두글자를 반환
+ * Level - easy
+ * 
+ * */
+
+public class WordGetmiddleEasy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("단어를 입력해주세요!");
+		String word=sc.nextLine();
+		int word_length=word.length();
+		int midValue=word_length/2;
+		String result=null;
+		
+		
+		if((word_length%2)==0)
+		{
+			result=word.substring(midValue-1, midValue+1);
+			
+		}
+		
+		if((word_length%2)==1)
+		{
+			result=word.substring(midValue, midValue+1);
+			
+		}
+			
+		
+		
+		
+		
+	}
+
+}
