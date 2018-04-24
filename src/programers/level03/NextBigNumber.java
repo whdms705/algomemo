d366f734b2e9c83708889bddc5712ad058305fdc
whdms705 <whdms705@nate.com>
2018-04-24 12:34:40 +0900

    Add comment

--- src/programers/level03/NextBigNumber.java
+++ src/programers/level03/NextBigNumber.java
@@ -1,5 +1,13 @@
 package programers.level03;
 
+
+/*
+ * N의 다음 큰 숫자는 N을 2진수로 바꾸었을 때의 1의 개수와 같은 개수로 이루어진 수입니다.
+1번째 조건을 만족하는 숫자들 중 N보다 큰 수 중에서 가장 작은 숫자를 찾아야 합니다.
+예를 들어, 78을 2진수로 바꾸면 1001110 이며, 78의 다음 큰 숫자는 83으로 2진수는 1010011 입니다.
+N이 주어질 때, N의 다음 큰 숫자를 찾는 nextBigNumber 함수를 완성하세요.
+ */
+
 public class NextBigNumber {
 	
 	public int nextBigNumber(int n)
