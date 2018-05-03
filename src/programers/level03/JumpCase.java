917ef6a2853cc7c7c405002db3aa359a6dbd2d67
whdms705 <whdms705@nate.com>
2018-05-03 12:51:21 +0900

    Update JumpCase practice
    Add JumpCase algo content

--- src/programers/level03/JumpCase.java
+++ src/programers/level03/JumpCase.java
@@ -2,9 +2,29 @@ package programers.level03;
 
 public class JumpCase {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int jumpCase(int num) {
+        int answer = 0;
+        if(num==1) {
+        	answer=1;
+        }else if(num==2) {
+        	answer=2;
+        }else {
+        	int temp1=1,temp2=1;
+	        for(int i=1;i<num;i++) {
+	        	answer = temp1 + temp2;
+	        	temp1=temp2;
+	        	temp2=answer;
+	        }
+        }
+System.out.println("answer : "+answer);
+        return answer;
+    }
 
-	}
+    public static void main(String[] args) {
+        JumpCase c = new JumpCase();
+        int testCase = 4;
+        //아래는 테스트로 출력해 보기 위한 코드입니다.
+        System.out.println(c.jumpCase(testCase));
+    }
 
 }
