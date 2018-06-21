60a47fa83f928437d21af4123b4fdc56fb9e6b34
whdms705 <whdms705@nate.com>
2018-06-22 01:32:30 +0900

    Create ThisBinarySearchTreeCheck.java

---
+++ src/hackerrank/CrackingtheCodingInterview/dataStructures/ThisBinarySearchTreeCheck.java
@@ -0,0 +1,10 @@
+package hackerRank.CrackingtheCodingInterview.dataStructures;
+
+public class ThisBinarySearchTreeCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
